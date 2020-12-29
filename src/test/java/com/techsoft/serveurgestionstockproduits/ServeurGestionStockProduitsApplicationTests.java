fd101755573ad0bdfb5cdd2bd2f1bc29ecc32bbf
theoteche <theoteche@gmail.com>
2020-12-29 12:21:08 +0100

    first commit

---
+++ src/test/java/com/techsoft/serveurgestionstockproduits/ServeurGestionStockProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techsoft.serveurgestionstockproduits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurGestionStockProduitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
